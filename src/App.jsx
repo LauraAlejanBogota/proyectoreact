24b236902ab52169f9d439ce8e6977f800035d82
Laura bogota <lauralejab96@gmail.com>
2021-09-12 21:46:00 -0500

    cliente real 2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Hola from './components/Hola'
+import React from 'heder';
+
+function App() {
+  <div className ="App">
+    <h1> Hola </h1>
+  </div>
+}
+  
+    
+
+
+export default App;
---
+++ src/components/Hola.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hola(){
+    return(
+        <div>Hola aleja soy el div del componente</div>
+    )
+}
+
+export default Hola
